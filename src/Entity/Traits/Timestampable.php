efce3072d64c742e0a1d8bf227cb190350006515
Yohann <yohann.hommet@outlook.fr>
2021-01-18 15:01:15 +0100

    Add: SecurityController with login, ProfileController and PasswordResetController

--- src/Entity/Traits/Timestampable.php
+++ src/Entity/Traits/Timestampable.php
@@ -3,23 +3,18 @@
 namespace App\Entity\Traits;
 
 
+use DateTimeImmutable;
 use DateTimeInterface;
 use Doctrine\ORM\Mapping as ORM;
 
 
-/**
- * Trait Timestampable
- *
- * @package App\Entity\Traits
- * @ORM\HasLifecycleCallbacks()
- */
 trait Timestampable
 {
 
     /**
      * @ORM\Column(type="datetime", options={"default": "CURRENT_TIMESTAMP"})
      */
-    private ?DateTimeInterface $createdAt;
+    private ?DateTimeInterface $createdAt = null;
 
     /**
      * @ORM\Column(type="datetime", options={"default": "CURRENT_TIMESTAMP"})
@@ -34,36 +29,41 @@ trait Timestampable
     {
         return $this->createdAt;
     }
-    /**
-     * @ORM\PrePersist()
-     * @ORM\PreUpdate()
-     * @param \DateTimeInterface|null $createdAt
-     * @return \App\Entity\Traits\Timestampable
-     */
-    public function setCreatedAt(?DateTimeInterface $createdAt): self
+
+
+    public function setCreatedAt(DateTimeInterface $createdAt): self
     {
         $this->createdAt = $createdAt;
+
         return $this;
     }
 
 
-    /**
-     * @return \DateTimeInterface|null
-     */
     public function getUpdatedAt(): ?DateTimeInterface
     {
         return $this->updatedAt;
     }
-    /**
-     * @ORM\PrePersist()
-     * @ORM\PreUpdate()
-     * @param \DateTimeInterface|null $updatedAt
-     * @return \App\Entity\Traits\Timestampable
-     */
-    public function setUpdatedAt(?DateTimeInterface $updatedAt): self
+
+
+    public function setUpdatedAt(DateTimeInterface $updatedAt): self
     {
         $this->updatedAt = $updatedAt;
+
         return $this;
+    }
+
+
+    /**
+     * @ORM\PrePersist
+     * @ORM\PreUpdate
+     */
+    public function updateTimestamps()
+    {
+        if ($this->getCreatedAt() === null) {
+            $this->setCreatedAt(new DateTimeImmutable());
+        }
+
+        $this->setUpdatedAt(new DateTimeImmutable());
     }
 
 }
